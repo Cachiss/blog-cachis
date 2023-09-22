ed7f4b7f09179b513f2970328c29c1373565df58
cachis <marcoscachis@gmail.com>
2023-09-22 16:53:51 -0600

    remove server side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,4 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
-  adapter: vercel()
 });
