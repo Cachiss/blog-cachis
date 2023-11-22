1f55b2f813eb9b27e878e662cca2ad1b4b0b3748
cachis <marcoscachis@gmail.com>
2023-11-22 02:02:11 -0600

    add support to tsx

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -4,6 +4,12 @@ export default {
   plugins: ["prettier-plugin-astro"],
   overrides: [
     {
+      files: ["*.ts", "*.js"],
+      options: {
+        parser: "babel",
+      },
+    },
+    {
       files: "*.astro",
       options: {
         parser: "astro",
